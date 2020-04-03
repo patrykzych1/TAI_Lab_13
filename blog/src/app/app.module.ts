b0f9a6fb5d43de646e6e25e09fa78a5d2ac4b889
Patryk Zych <patzycc@gmail.com>
2020-04-03 11:55:25 +0200

    add components

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BlogComponent } from './components/blog/blog.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { ScrollComponent } from './components/scroll/scroll.component';
+import { BlogItemComponent } from './components/blog-item/blog-item.component';
+import { BlogItemTextComponent } from './components/blog-item-text/blog-item-text.component';
+import { BlogItemImageComponent } from './components/blog-item-image/blog-item-image.component';
+import { BlogItemDetailsComponent } from './components/blog-item-details/blog-item-details.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +21,10 @@ import { ScrollComponent } from './components/scroll/scroll.component';
     ContactComponent,
     NavbarComponent,
     ScrollComponent,
+    BlogItemComponent,
+    BlogItemTextComponent,
+    BlogItemImageComponent,
+    BlogItemDetailsComponent,
   ],
   imports: [
     BrowserModule,
